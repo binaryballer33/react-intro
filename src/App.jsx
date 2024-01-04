65004c3e7fa5971ae3e7eecf51fc8fa31adf5573
Shaquille Mandy <shaqmandy@gmail.com>
2024-01-04 14:39:07 -0300

    added puppy data and displayed puppies on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
 
   return (
     <>
+      { 
+        puppyList.map((puppy) => {
+          return <p key={puppy.key}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
