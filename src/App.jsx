becd9c02240f7761ab16e0ca200b5238557ea61b
Shaquille Mandy <shaqmandy@gmail.com>
2024-01-04 14:40:40 -0300

    added state to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
 import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
   return (
-    <>
-      { 
-        puppyList.map((puppy) => {
-          return <p key={puppy.key}>{puppy.name}</p>
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>;
         })
       }
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
